47b31ba537f9c7eaf914fdeadeda685ffe027c6d
VijayMayhul <vijaymayhul@gmail.com>
2024-03-05 20:24:11 +0530

    Updating File-System Task Files

--- index.js
+++ index.js
@@ -23,12 +23,14 @@ app.get('/', (req, res)=>{
     <h1 style="background-color:skyblue;padding:10px 0px;text-align:center">
     Express Server is Connected !</h1>
     <div style="text-align:center">
-    <p>To create txt file enter, <span style="background-color:yellow;color:blue">/new-create-write-read-txt</span> in the URL</p>
-    <p>To Retrieve all txt file enter, <span style="background-color:yellow;color:blue">/read-all-txtFiles</span> in the URL</p>
+    <p><span style="background-color:lightgreen;font-size:18px"> To Create a New txt file</span> --> <a href="/new-create-write-read-txt">Click Here</a></p>
+    <p><span style="background-color:skyblue;font-size:18px">To Retrieve All txt file</span> --> <a href="/read-all-txtFiles">Click Here</a></p>
     </div>
     `);
     
 });
+
+
 
 
 //Creating new data-time.txt file andread it's inside content
@@ -56,6 +58,7 @@ app.get('/new-create-write-read-txt',(req, res)=>{
         <h1>File created successfully!</h1>
         <p>Txt File Name : ${currentTimeStamp}.txt</p>
         <p>Txt File Content : ${data}</p>
+        <p><a href="/" style="color:yellow">Back to Home</a></p>
         </div>`);
     }catch(err){
 
@@ -79,9 +82,9 @@ app.get('/read-all-txtFiles', (req, res) => {
 
         // Sending response status and data
         res.status(200).send(`<h1 style="background-color:blue;padding:10px 0px;text-align:center;color:white">
-        All Retrieved Text Files</h1><ul>${txtFiles.map(file => 
+        All Retrieved Text Files<p><a href="/" style="color:yellow">Back to Home</a></p></h1><ul>${txtFiles.map(file => 
             `<dl style="display: list-item;list-style-type: disc;">
-            <dt ><b>File Name : </b>${file}</dt>
+            <dt><b>File Name : </b>${file}</dt>
             <dd style="display: list-item;list-style-type: circle;"><b>File Content : </b>${fs.readFileSync(`./TimeStamp/${file}`, 'utf8')}</dd>
             </dl>`).join('')}</ul>`);
     } catch (err) {
